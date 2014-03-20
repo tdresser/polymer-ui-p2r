e5244fe603369a438b5e96f74a69eddb7f1d3540
Tim Dresser <tdresser@chromium.org>
2014-03-20 13:48:13 -0400

    friction is hard

--- polymer-p2r.js
+++ polymer-p2r.js
@@ -1,14 +1,30 @@
+// Try only setting friction on draw, and then reading the offset out of the overscroll object when finding start.
+
+var MAX_OFFSET = 400;
+
+function addFriction(delta) {
+  if (delta < 0) {
+    return delta;
+  }
+  var scale = 1;
+  delta /= scale;
+
+  // We want a curve that starts out linear, and slopes down
+  // to slope=0 by maxDelta.
+  var adj = delta - delta*delta/(2*MAX_OFFSET);
+  return adj;
+}
+
 function Overscroll() {
   var self = this;
   var d = 0;
   var target = 0;
-
   this.setTarget = function(t) {
     target = t;
   }
 
   this.reachedTarget = function() {
-    if (Math.abs(d - this.target) < 0.5) {
+    if (Math.abs(d - this.target) < 1) {
       return true;
     }
   }
@@ -22,8 +38,9 @@ function Overscroll() {
   }
 
   this.setOffset = function(o) {
-    d = o;
-    target = o;
+//    console.log("setOffset " + o);
+    d = Math.max(0, Math.min(o, MAX_OFFSET));
+    target = d;
   }
 
   this.getOffset = function() {
@@ -43,8 +60,6 @@ Polymer('polymer-p2r', {
     var loadingOffset = 50;
     var seenTouchMoveThisSequence = false;
     var fingersDown = 0;
-    var maxOffset = 200;
-
     var overscroll = new Overscroll();
 
     function getHeaderClassName(name) {
@@ -57,7 +72,6 @@ Polymer('polymer-p2r', {
 
     function translateY(element, offset) {
       element.style.webkitTransform = 'translate3d(0, ' + offset + 'px, 0)';
-      window.getComputedStyle(element);
     }
 
     function checkPulled() {
@@ -71,8 +85,14 @@ Polymer('polymer-p2r', {
       framePending = false;
       checkPulled();
       overscroll.step();
-      translateY(scrollcontent, addFriction(overscroll.getOffset()));
-      translateY(p2r, addFriction(overscroll.getOffset()) - p2r.clientHeight);
+      var offset = overscroll.getOffset();
+      if (offset < 0) {
+        offset = 0;
+//        scroller.scrollTop = -offset;
+//        console.log("Set scrollTop to " + -offset);
+      }
+      translateY(scrollcontent, overscroll.getOffset());
+      translateY(p2r, overscroll.getOffset() - p2r.clientHeight);
       if (!overscroll.reachedTarget()) {
         scheduleUpdate();
       }
@@ -85,16 +105,6 @@ Polymer('polymer-p2r', {
       }
     }
 
-    function addFriction(delta) {
-      return delta;
-      var scale = 2;
-      delta /= scale;
-
-      // We want a curve that starts out linear, and slopes down
-      // to slope=0 by maxDelta.
-      var adj = delta - delta*delta/(2*maxOffset);
-      return adj;
-    }
 
     function isP2rVisible() {
       return scroller.scrollTop <= overscroll.getOffset();
@@ -103,7 +113,7 @@ Polymer('polymer-p2r', {
     function isPulling() {
 //      console.log("overscroll.getOffset is " + overscroll.getOffset());
 //      return overscroll.getOffset() > 0 || overscrollOffset > 0;
-      return overscroll.getOffset() > 0;
+      return overscroll.getOffset() > 0.2;
     }
 
     function finishPull(e) {
@@ -136,8 +146,21 @@ Polymer('polymer-p2r', {
 
     scroller.addEventListener('touchstart', function(e) {
       lastY = e.touches[0].screenY;
+      pullStartY = lastY;
       fingersDown++;
       seenTouchMoveThisSequence = false;
+
+      if (isPulling()) {
+        console.log("CONTINUE PULL");
+        pullStartY = e.touches[0].screenY - overscroll.getOffset();
+        console.log("pullStartY " + pullStartY);
+        console.log("lastOffsetPreFriction " + lastOffsetPreFriction);
+        var offset = e.touches[0].screenY - pullStartY;
+        overscroll.setOffset(offset);
+        console.log("SET OFFSET TO " + offset);
+        seenTouchMoveThisSequence = true;
+      }
+
 //      if (e.touches.length == 1 && !isP2rVisible()) {
 //        if (getHeaderClassName() != '') {
 //          console.log("Reset for touchstart");
@@ -155,35 +178,31 @@ Polymer('polymer-p2r', {
       var scrollDelta = lastY - e.touches[0].screenY;
       var startingNewPull = !isPulling() && scroller.scrollTop <= 0 && scrollDelta < 0;
 
-      if (isPulling() && !seenTouchMoveThisSequence) {
-        console.log("CONTINUE PULL");
-        pullStartY = lastY - overscroll.getOffset();
-        seenTouchMoveThisSequence = true;
-        return;
-      }
-
       if (startingNewPull) {
         console.log("STARTING NEW PULL at " + e.touches[0].screenY);
         // Can't use lastY, it's invalid if you wiggle up and down enough
         pullStartY = e.touches[0].screenY - 1;
+      } else if (!seenTouchMoveThisSequence) {
+        return;
       }
 
       lastY = e.touches[0].screenY;
 //      console.log("CUR POSITION IS " + e.touches[0].screenY);
 
       if (!startingNewPull && !isPulling()) {
-        console.log("GET OUT");
-        return;
+//        console.log("GET OUT");
+//        return;
       }
 
-//      console.log("offset is " + offset);
-
       var offset = e.touches[0].screenY - pullStartY;
-      overscroll.setOffset(Math.max(0, Math.min(offset, maxOffset)));
+      console.log("offset is " + offset);
+      lastOffsetPreFriction = offset;
+//      overscroll.setOffset(addFriction(offset));
+      overscroll.setOffset(offset);
       scheduleUpdate();
 
       if (seenTouchMoveThisSequence && offset > 0) {
-        console.log("preventDefault");
+//        console.log("preventDefault");
         // Don't preventDefault the first touchMove, it would prevent
         // scroll from occurring.
         e.preventDefault();
