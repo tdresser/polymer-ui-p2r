51cf83cf4cf006744feb21f4bb7a951f9b29e72c
Tim Dresser <tdresser@chromium.org>
2014-04-28 10:41:39 -0400

    push for testing

--- polymer-p2r.js
+++ polymer-p2r.js
@@ -53,8 +53,8 @@ function Overscroll() {
       d = this.MAX_OFFSET;
     }
 
-    var dist_to_target = (target - d) / 13;
-    var spring = -10 * GRAVITY * (1 / (dist_to_target * dist_to_target + 1));
+    var dist_to_target = (target - d) / 20;
+    var spring = -2 * GRAVITY * (1 / (dist_to_target * dist_to_target + 1));
 //    var spring = -1.5 * GRAVITY * (1 / (Math.abs(dist_to_target / 100) + 1));
 
     var a = 0;
