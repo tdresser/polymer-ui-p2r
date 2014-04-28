b36f9184d8538524505641ab0b344ba2cb4fa0c1
Tim Dresser <tdresser@chromium.org>
2014-04-28 14:44:38 -0400

    Push for testing

--- polymer-p2r.js
+++ polymer-p2r.js
@@ -73,8 +73,8 @@ function Overscroll() {
       console.log("distance is " + (d - target));
       // Smoothly switch between using gravity model and spring model.
       var lerp = 1;
-      if (v < 10) {
-         lerp = (d - target) / 10;
+      if (v < 100) {
+         lerp = (d - target) / 100;
       }
       console.log("before " + a);
       console.log("lerp a " + a * lerp);
