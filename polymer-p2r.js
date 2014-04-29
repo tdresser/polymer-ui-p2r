8108cb3d422360b651a2d334071fff9738e26e8f
Tim Dresser <tdresser@chromium.org>
2014-04-29 16:42:06 -0400

    Making minimal testcase

--- polymer-p2r.js
+++ polymer-p2r.js
@@ -206,8 +206,9 @@ Polymer('polymer-p2r', {
 
       if (overscroll.getOffset() <= 0) {
         console.log("Repair offset " + overscroll.getOffset());
+        console.log("current scrollTop is " + scroller.scrollTop);
         scroller.scrollTop = -overscroll.getOffset();
-//        pullStartY -= 2 * overscroll.getOffset();
+        pullStartY -= overscroll.getOffset();
         overscroll.setOffset(0);
       }
 //      translateY(scrollcontent, overscroll.addFriction(overscroll.getOffset()));
